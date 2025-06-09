287213881e429b8e15cce14f8da61b805ef701ad
Victor Assis <46487019+victorlga@users.noreply.github.com>
2025-06-09 19:21:03 -0300

    docs: update license to MIT

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
 (defproject payment-webhook-handler "0.1.0-SNAPSHOT"
   :description "A Clojure webhook handler for processing payment events"
   :url "https://github.com/victorlga/payment-webhook-handler"
-  :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
-            :url "https://www.eclipse.org/legal/epl-2.0/"}
+  :license {:name "MIT"
+            :url "https://opensource.org/licenses/MIT"})
   :dependencies [[org.clojure/clojure "1.11.1"]
                  [org.xerial/sqlite-jdbc "3.45.1.0"]
                  [org.clojure/java.jdbc "0.7.12"]
